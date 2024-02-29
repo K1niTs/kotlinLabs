109528f1ae6e8e45d42bfc27eca5d00bcba1d2ed
K1niTs <koletwinov.denis@yandex.ru>
2024-03-01 01:44:10 +0300

    complete

---
+++ src/main/kotlin/lab5.kt
@@ -0,0 +1,8 @@
+
+fun main() {
+    val scan = java.util.Scanner(System.`in`)
+    println("Вводим")
+    val number: Int = scan.nextInt()
+    val findNumber = "${number}${number * 2}"
+    println("Получаем: $findNumber")
+}
---
+++ src/main/kotlin/lab6.kt
@@ -0,0 +1,10 @@
+
+fun main() {
+    val scan = java.util.Scanner(System.`in`)
+    println("Введите ваше имя: ")
+    val name: String = scan.nextLine()
+    println("Введите ваш возраст: ")
+    val age: Int = scan.nextInt()
+    println("Привет, $name! Тебе уже $age лет.")
+
+}
