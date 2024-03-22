66261dd90684776267d64cab910eb1bfce88ad76
K1niTs <koletwinov.denis@yandex.ru>
2024-03-22 11:15:16 +0300

    update 4lab

--- src/main/kotlin/4lab3.kt
+++ src/main/kotlin/4lab3.kt
@@ -1,7 +1,7 @@
 //24,-63,67,-12,88,94,-28,82,0,53
 
 fun main() {
-    val array = readLine()?.split(",")?.map { it.toInt() }?.toMutableList()
+    val array = readLine()?.split(",")?.map { it.toInt() }
 
     if (array != null && array.isNotEmpty()) {
         val min = array.min()
