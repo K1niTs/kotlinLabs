fbf1fc16092f60668570787024fb6f9367bf093f
K1niTs <koletwinov.denis@yandex.ru>
2024-03-22 09:13:56 +0300

    update 4lab

--- src/main/kotlin/4lab3.kt
+++ src/main/kotlin/4lab3.kt
@@ -3,8 +3,8 @@
 fun main() {
     val array = mutableListOf(24, -63, 67, -12, 88, 94, -28, 82, 0, 53)
 
-    val min = array.minOrNull()
-    val max = array.maxOrNull()
+    val min = array.min()
+    val max = array.max()
 
     println("Минимальный элемент $min")
     println("Максимальный элемент: $max")
