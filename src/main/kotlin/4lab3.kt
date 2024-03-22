8e30a3c4c520d73f26b8914e54f830b7b0deabbd
K1niTs <koletwinov.denis@yandex.ru>
2024-03-22 11:11:12 +0300

    update 4lab

--- src/main/kotlin/4lab3.kt
+++ src/main/kotlin/4lab3.kt
@@ -1,11 +1,15 @@
-
+//24,-63,67,-12,88,94,-28,82,0,53
 
 fun main() {
-    val array = mutableListOf(24, -63, 67, -12, 88, 94, -28, 82, 0, 53)
+    val array = readLine()?.split(",")?.map { it.toInt() }?.toMutableList()
 
-    val min = array.min()
-    val max = array.max()
+    if (array != null && array.isNotEmpty()) {
+        val min = array.min()
+        val max = array.max()
 
-    println("Минимальный элемент $min")
-    println("Максимальный элемент: $max")
+        println("Минимальный элемент: $min")
+        println("Максимальный элемент: $max")
+    } else {
+        println("Не правильно набрано")
+    }
 }
