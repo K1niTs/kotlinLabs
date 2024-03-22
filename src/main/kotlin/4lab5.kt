d9bb013fba9064dcf6dabf4a0518dcc53bb62b93
K1niTs <koletwinov.denis@yandex.ru>
2024-03-22 09:26:01 +0300

    update 4lab

--- src/main/kotlin/4lab5.kt
+++ src/main/kotlin/4lab5.kt
@@ -1,7 +1,7 @@
 //24,-63,67,-12,88,94,-28,82,0,53
 
 fun main() {
-    val array = readLine()?.split(",")?.map { it.trim().toInt() }?.sorted()
+    val array = readLine()?.split(",")?.map { it.toInt() }?.sorted()
 
     if (array != null && array.isNotEmpty()) {
         println("Отсортированный массив: ${array.joinToString()}")
